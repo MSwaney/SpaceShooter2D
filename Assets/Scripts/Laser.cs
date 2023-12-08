ec4a7542c644388c67399bd893391363ae70bc6e
Matthew Swaney <Phiktional@Gmail.com>
2023-12-07 22:30:05 -0800

    Added damage of player and destruction of lasers, enemies and player

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    //speed variable of 8
     [SerializeField]
     private float _speed = 8f;
 
-    // Update is called once per frame
     void Update()
     {
         CalculateMovement();
