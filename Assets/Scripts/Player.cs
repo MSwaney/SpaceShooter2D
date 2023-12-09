e67f42f3336c5259a5d7b9f0ebe86b8a8dd295a5
Matthew Swaney <Phiktional@Gmail.com>
2023-12-08 23:19:48 -0800

    Added spawn manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,10 +13,17 @@ public class Player : MonoBehaviour
     private float _canFire = -1f;
     [SerializeField]
     private int _lives = 3;
+    private SpawnManager _spawnManager;
 
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if (_spawnManager == null )
+        {
+            Debug.LogError("Spawn Manager is NULL.");
+        }
     }
 
     void Update()
@@ -68,6 +75,9 @@ public class Player : MonoBehaviour
         
         if (_lives < 1)
         {
+            //Communicate with Spawn Manager
+            _spawnManager.OnPlayerDeath();
+            //Let them know to stop spawning
             Destroy(this.gameObject);
         }
     }
